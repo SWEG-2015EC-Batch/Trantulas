d9de5f133b4f7acbb1cfd0815cc9ad9830217c8c
MisganaBetselot <149225863+MisganaBetselot@users.noreply.github.com>
2023-11-17 15:42:10 +0300

    Update BMI.cpp

--- ETS0961_15_Misgana_Betselot/BMI.cpp
+++ ETS0961_15_Misgana_Betselot/BMI.cpp
@@ -1,8 +1,9 @@
+//This program is written for calculating the body mass index of a person
 #include <iostream>
 using namespace std;
 int main()
 {
-  double Weight,Height,BMI;
+  double Weight,Height,BMI;//BMI is body mass index
     cout<<"Enter Weight(in Kg)";
     cin>> Weight;
     cout<<"Enter Height(in M)";
