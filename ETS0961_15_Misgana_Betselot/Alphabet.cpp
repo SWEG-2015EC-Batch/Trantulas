c1f957eb07744786bd30e2c2b9e565453e912b72
MisganaBetselot <149225863+MisganaBetselot@users.noreply.github.com>
2023-11-17 20:36:08 +0300

    Update Alphabet.cpp

--- ETS0961_15_Misgana_Betselot/Alphabet.cpp
+++ ETS0961_15_Misgana_Betselot/Alphabet.cpp
@@ -1,1 +1,23 @@
+//This is a program written to convert uppercase and lowercase letters to their equivalent
+#include <iostream>
+#include <cctype>
+using namespace std;
+int main() {
+    char letter;
+    cout << "Enter a letter: ";
+    cin >> letter;
 
+    if (isupper(letter)) {
+        char lowercase =tolower(letter);
+        cout << "Lowercase equivalent: " << lowercase <<endl;
+    }
+    else if (islower(letter)) {
+        char uppercase = toupper(letter);
+        cout << "Uppercase equivalent: " << uppercase << endl;
+    }
+    else {
+        cout << "Invalid input! Please enter a valid letter." << endl;
+    }
+
+    return 0;
+}
