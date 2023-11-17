e17e437fed26d1a499b7c410275b398944543782
mihretTesfaye <149238178+mihretTesfaye@users.noreply.github.com>
2023-11-17 08:48:21 -0800

    Update uppercase lowercase changer.cpp

--- ETS0927_15_Mihret_Tesfaye/uppercase lowercase changer.cpp
+++ ETS0927_15_Mihret_Tesfaye/uppercase lowercase changer.cpp
@@ -1,21 +1,26 @@
-//the program convert uppercase to lowercase or vice versa
+ // for uppercase and lowercase functions
 #include <iostream>
-#include<ctype.h>
+#include <cctype>
 using namespace std;
-
 int main() {
     char letter;
     cout << "Enter a letter: ";
     cin >> letter;
-  if (letter >= 'a' && letter <= 'z') {
-       letter = letter - 32;
-        cout << "Uppercase equivalent: " << letter << endl;
-    } else if (letter >= 'A' && letter <= 'Z') {
-       letter = letter + 32;
-    cout << "Lowercase equivalent: " << letter << endl;
-    } else {
-    cout << "Invalid input. Please enter a valid letter." << endl;
+
+    if (isupper(letter)) {
+        char lowercase =tolower(letter);
+        cout << "Lowercase equivalent: " << lowercase <<endl;
+    }
+    else if (islower(letter)) {
+        char uppercase = toupper(letter);
+        cout << "Uppercase equivalent: " << uppercase << endl;
+    }
+    else {
+        cout << "Invalid input! Please enter a valid letter." << endl;
     }
 
     return 0;
 }
+
+
+
