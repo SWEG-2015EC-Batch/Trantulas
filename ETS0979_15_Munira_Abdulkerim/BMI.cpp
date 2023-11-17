1651bc817894700daf46943a6e4b797c4f01ef2d
MuniraAbdulkerim <149477076+MuniraAbdulkerim@users.noreply.github.com>
2023-11-17 17:39:58 +0000

    Update BMI.cpp

--- ETS0979_15_Munira_Abdulkerim/BMI.cpp
+++ ETS0979_15_Munira_Abdulkerim/BMI.cpp
@@ -9,6 +9,6 @@ int main()
     cout<<"Enter Height(in M)";
     cin>> Height;
     BMI=Weight/(Height*Height);
-    cout<<"The Result of your BMI is;"<<BMI;
+    cout<<"The Result of your BMI is"<<BMI;
     return 0;
 }
