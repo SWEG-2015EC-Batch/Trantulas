dccacdf4f1f8f69a51e5862ab40ee1c3106769a5
MuniraAbdulkerim <149477076+MuniraAbdulkerim@users.noreply.github.com>
2023-11-17 20:31:36 +0000

    Update file transmission.cpp

--- ETS0979_15_Munira_Abdulkerim/file transmission.cpp
+++ ETS0979_15_Munira_Abdulkerim/file transmission.cpp
@@ -1,1 +1,13 @@
-
+// the program that ccalculate file transmission in byte
+#include <iostream>
+using namespace std;
+int main() {
+const int transmissionRate = 960;
+   int fileSizeInBytes;
+    int timeInSeconds;
+  cout << "Enter the file size in bytes: ";
+  cin >> fileSizeInBytes;
+timeInSeconds = fileSizeInBytes / transmissionRate;
+cout << "Time required to send the file: " << timeInSeconds << " seconds" << endl;
+return 0;
+}
