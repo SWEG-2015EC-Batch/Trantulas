a47540edfac996188d14c66b4cdbe4ea3430ae97
MisganaBetselot <149225863+MisganaBetselot@users.noreply.github.com>
2023-11-17 20:50:25 +0300

    Update Filesize.cpp

--- ETS0961_15_Misgana_Betselot/Filesize.cpp
+++ ETS0961_15_Misgana_Betselot/Filesize.cpp
@@ -1,1 +1,13 @@
-
+//This is a program that calculates the time taken to send a file in bytes
+#include <iostream>
+using namespace std;
+int main() {
+const int transmissionRate = 960;
+   int fileSizeInBytes;
+    int timeInSeconds;
+  cout << "Enter the file size in bytes: ";
+  cin >> fileSizeInBytes;
+timeInSeconds = fileSizeInBytes / transmissionRate;
+cout << "Time required to send the file: " << timeInSeconds << " seconds" << endl;
+return 0;
+}
