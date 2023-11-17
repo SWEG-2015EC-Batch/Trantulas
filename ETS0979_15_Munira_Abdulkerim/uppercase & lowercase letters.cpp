b27cb70c5143f9ac79c32f92bffacd60a7a32f1a
MuniraAbdulkerim <149477076+MuniraAbdulkerim@users.noreply.github.com>
2023-11-17 20:20:01 +0000

    Update uppercase & lowercase letters.cpp

--- ETS0979_15_Munira_Abdulkerim/uppercase & lowercase letters.cpp
+++ ETS0979_15_Munira_Abdulkerim/uppercase & lowercase letters.cpp
@@ -1,1 +1,27 @@
+//this program convert uppercase to lower case and viceversa
+#include<iostream>
+#include<ctype.h>
+using namespace std;
+int main()
+{
+    char Letter;
+    cout<<"Enter the Letter"<< endl;
+    cin>>Letter;
+    if (islower (Letter))
+        {
+            Letter = toupper(Letter);
+            cout<< " the upper case letter is" <<Letter;
+        }
+    else if (isupper(Letter))
+		{
+		Letter = tolower(Letter);
+		cout<< " the lower case letter is" <<Letter;
+		}
+	else
+		{
+        cout << "The character " << Letter << " is not a letter."<< endl;
+    	}
+
+    return 0;
+}
 
