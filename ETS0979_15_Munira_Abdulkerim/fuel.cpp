53f2904ade89ca6db54574b5eeb7928a90d72e99
MuniraAbdulkerim <149477076+MuniraAbdulkerim@users.noreply.github.com>
2023-11-17 16:59:58 +0000

    Update fuel.cpp

--- ETS0979_15_Munira_Abdulkerim/fuel.cpp
+++ ETS0979_15_Munira_Abdulkerim/fuel.cpp
@@ -1,1 +1,17 @@
+//This is a program written for calculating the number of miles that an automobile can be driven without refueling.
+#include <iostream>
+using namespace std;
+int main() {
+    double fuelCapacity, mpg;//mpg is the mile per gallon
 
+    cout << "Enter the fuel tank capacity in gallons: ";
+    cin >> fuelCapacity;
+
+    cout << "Enter the miles per gallon (MPG) of the automobile: ";
+    cin >> mpg;
+    double maxDistance = fuelCapacity * mpg;
+
+    cout << "The automobile can be driven for " << maxDistance << " miles without refueling." <<endl;
+
+    return 0;
+}
