76964594b9a409d4f42a996ee23033af6f88c878
lydiayonas <149570976+lydiayonas@users.noreply.github.com>
2023-11-18 09:19:57 +0300

    Add files via upload

---
+++ ETS0848_15_Lydia_Yonas/exponetial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x, y;
+    cout << "Enter the value of x: ";
+    cin >> x;
+    cout << "Enter the value of y: ";
+    cin >> y;
+    double result = pow(x, y);
+    cout << "The result of  x^y is:" << result <<endl;
+    return 0;
+}
---
+++ ETS0848_15_Lydia_Yonas/upercase lowercase.cpp
@@ -0,0 +1,26 @@
+ // for uppercase and lowercase functions
+#include <iostream>
+#include <cctype>
+using namespace std;
+int main() {
+    char letter;
+    cout << "Enter a letter: ";
+    cin >> letter;
+
+    if (isupper(letter)) {
+        char lowercase =tolower(letter);
+        cout << "Lowercase equivalent: " << lowercase <<endl;
+    }
+    else if (islower(letter)) {
+        char uppercase = toupper(letter);
+        cout << "Uppercase equivalent: " << uppercase << endl;
+    }
+    else {
+        cout << "Invalid input! Please enter a valid letter." << endl;
+    }
+
+    return 0;
+}
+
+
+
