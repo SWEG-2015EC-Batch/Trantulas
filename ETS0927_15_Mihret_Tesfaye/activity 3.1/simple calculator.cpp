71c0d37531c1cbbcea22e45600d7e1a4ab7ad06e
mihretTesfaye <149238178+mihretTesfaye@users.noreply.github.com>
2023-12-04 00:15:54 -0800

    Create simple calculator.cpp

---
+++ ETS0927_15_Mihret_Tesfaye/activity 3.1/simple calculator.cpp
@@ -0,0 +1,44 @@
+//simple calculator
+#include<iostream>
+using namespace std;
+int main() {
+    char op,exitcode;//op stands for operators
+    float num1, num2;
+    do {
+
+        cout << "Enter the first number: ";
+        cin >> num1;
+        cout << "Enter operator: +, -, *, /: ";
+        cin >> op;
+        cout << "Enter the second number: ";
+        cin >> num2;
+
+        switch(op) {
+            case '+':
+                cout << num1+num2 << endl;
+                break;
+
+            case '-':
+                cout << num1-num2 << endl;
+                break;
+
+            case '*':
+                cout << num1*num2 << endl;
+                break;
+
+            case '/':
+                if(num2 != 0.0)
+                    cout << num1/num2 << endl;
+                else
+                    cout << "undefined opration!" << endl;
+                break;
+
+            default:
+                cout << "Invalid operator!" << endl;
+                break;
+        }
+        cout << "Enter 0 to exit, any other key to continue: ";
+        cin >> exitcode;
+    } while(exitcode != '0');
+    return 0;
+}
