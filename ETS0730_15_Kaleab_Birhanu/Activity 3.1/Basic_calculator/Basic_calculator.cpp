993a364caac94be22b065851372fd9466534d8ca
Kaleab-Birhanu <149373883+Kaleab-Birhanu@users.noreply.github.com>
2023-12-17 22:41:38 +0300

    Update Basic_calculator.cpp

--- ETS0730_15_Kaleab_Birhanu/Activity 3.1/Basic_calculator/Basic_calculator.cpp
+++ ETS0730_15_Kaleab_Birhanu/Activity 3.1/Basic_calculator/Basic_calculator.cpp
@@ -1,60 +1,46 @@
-//basic arithmetic calculator 
-#include <iostream>
+// basic arithmetic calculator
+#include<iostream>
 using namespace std;
 
 int main() {
-    int num_1, num_2, result, status {0};
-    char operate;
-
+    char op,exitcode;//here op stands for operator
+    float num1, num2;
     do {
-        cout << "Please enter the first number: ";
-        cin >> num_1;
-        cout << "Please enter the second number: ";
-        cin >> num_2;
 
-        bool validOperator = false;
-        while (!validOperator) {
-            cout << "Enter the type of operation you want, (+ , - , * , / ) or (r) if you want to calculate remainder: ";
-            cin >> operate;
+        cout << "Enter the first number: ";
+        cin >> num1;
+        cout << "Enter operator: (+, -, *, /)   ";
+        cin >> op;
+        cout << "Enter the second number: ";
+        cin >> num2;
 
-            switch (operate) {
-                case '+': 
-                    result = num_1 + num_2;
-                    validOperator = true;
-                    break;
-                case '-': 
-                    result = num_1 - num_2;
-                    validOperator = true;
-                    break;
-                case '*': 
-                    result = num_1 * num_2;
-                    validOperator = true;
-                    break;
-                case '/': 
-                    if (num_2 != 0) {
-                        result = num_1 / num_2;
-                        validOperator = true;
-                    } else {
-                        cout << "Error: Division by zero is not allowed.\n";
-                    }
-                    break;
-                case 'R': case 'r': 
-                    if (num_2 != 0) {
-                        result = num_1 % num_2;
-                        validOperator = true;
-                    } else {
-                        cout << "Error: Division by zero is not allowed.\n";
-                    }
-                    break;
-                default: 
-                    cout << "Unknown operator: " << operate << '\n';
-            }
+        switch(op) {
+            case '+':
+                cout << num1+num2 << endl;
+                break;
+
+            case '-':
+                cout << num1-num2 << endl;
+                break;
+
+            case '*':
+                cout << num1*num2 << endl;
+                break;
+
+            case '/':
+                if(num2 != 0.0)
+                    cout << num1/num2 << endl;
+                else
+                    cout << "undefined operation !" << endl;
+                break;
+
+            default:
+                cout << "Invalid operator!" << endl;
+                break;
         }
-
-        cout << num_1 << operate << num_2 << " = " << result << endl;
-        cout << "Please enter 0 to quit or any other number to continue: ";
-        cin >> status;
-    } while (status != 0);
+        cout << "Enter 0 to exit, any other key to continue: ";
+        cin >> exitcode;
+    } while(exitcode != '0');
 
     return 0;
 }
