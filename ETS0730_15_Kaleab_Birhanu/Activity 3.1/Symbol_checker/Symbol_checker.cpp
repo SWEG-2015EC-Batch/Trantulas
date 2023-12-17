af48a64869203f8c456c2dce7223b2a79c5a2e4d
Kaleab-Birhanu <149373883+Kaleab-Birhanu@users.noreply.github.com>
2023-12-17 22:10:39 +0300

    Create Symbol_checker.cpp

---
+++ ETS0730_15_Kaleab_Birhanu/Activity 3.1/Symbol_checker/Symbol_checker.cpp
@@ -0,0 +1,38 @@
+// a program that tells whether a character entered from the keyboard is in upper case or in lower case or digit or special character
+#include <iostream>
+using namespace std;
+
+int main() {
+  char ch;
+  cout << "Enter a character: ";
+  cin >> ch;
+  if (isupper(ch)) {
+    cout << ch << " is an uppercase letter." << endl;
+    if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+      cout << "It is also a vowel." << endl;
+    } else {
+      cout << "It is a consonant." << endl;
+    }
+  }
+  else if (islower(ch)) {
+    cout << ch << " is a lowercase letter." << endl;
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+      cout << "It is also a vowel." << endl;
+    } else {
+      cout << "It is a consonant." << endl;
+    }
+  }
+  else if (isdigit(ch)) {
+    cout << ch << " is a digit." << endl;
+    if (ch % 2 == 0) {
+      cout << "It is an even number." << endl;
+    } else {
+      cout << "It is an odd number." << endl;
+    }
+  }
+  else {
+    cout << ch << " is a special character." << endl;
+  }
+
+  return 0;
+}
