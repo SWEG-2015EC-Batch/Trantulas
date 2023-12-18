872c3221cc4ce31e6fd5a0ed10943a14abe08307
Kaleab-Birhanu <149373883+Kaleab-Birhanu@users.noreply.github.com>
2023-12-18 19:54:26 +0300

    Update Income_calculator.cpp

--- ETS0730_15_Kaleab_Birhanu/Activity 3.1/Income_calculator/Income_calculator.cpp
+++ ETS0730_15_Kaleab_Birhanu/Activity 3.1/Income_calculator/Income_calculator.cpp
@@ -2,37 +2,37 @@
 #include <iostream>
 using namespace std;
 int main(){
-  float gross_salary,workedHour,taxRate,overTimeBonusRate,netSalary,bonusPayment=0;
-  const float pensionRate=0.07;
+  float grosssalary,workedhour,taxrate,overtimebonusrate,netsalary,bonuspayment=0;
+  const float pensionrate=0.07;
   cout<<"Enter Gross salary ";
-  cin>>gross_salary;
+  cin>>grosssalary;
   cout<<"Enter Worked hour ";
-  cin>>workedHour;
+  cin>>workedhour;
 
 
-  if(workedHour > 40){
+  if(workedhour > 40){
     cout<<"Enter over time bonus rate ";
-    cin>>overTimeBonusRate;
-    bonusPayment = overTimeBonusRate*(workedHour-40);
+    cin>>overtimebonusrate;
+    bonuspayment = overtimebonusrate*(workedhour-40);
 
 }
 
-    if (gross_salary <= 200) {
-        taxRate = 0.0;
-    } else if (gross_salary <= 600) {
-        taxRate = 0.1;
-    } else if (gross_salary <= 1200) {
-        taxRate = 0.15;
-    } else if (gross_salary <= 2000) {
-        taxRate = 0.2;
-    } else if (gross_salary <= 3500) {
-        taxRate = 0.25;
+    if (grosssalary <= 200) {
+        taxrate = 0.0;
+    } else if (grosssalary <= 600) {
+        taxrate = 0.1;
+    } else if (grosssalary <= 1200) {
+        taxrate = 0.15;
+    } else if (grosssalary <= 2000) {
+        taxrate = 0.2;
+    } else if (grosssalary <= 3500) {
+        taxrate = 0.25;
     } else {
-        taxRate = 0.3;
+        taxrate = 0.3;
     }
 
-  netSalary = (gross_salary - (pensionRate*gross_salary) - (taxRate*gross_salary))+ bonusPayment;
+  netsalary = (grosssalary - (pensionrate*grosssalary) - (taxrate*grosssalary))+ bonuspayment;
 
- cout<<"Net salary = "<<netSalary;
+ cout<<"Net salary = "<<netsalary;
  return 0;
 }
