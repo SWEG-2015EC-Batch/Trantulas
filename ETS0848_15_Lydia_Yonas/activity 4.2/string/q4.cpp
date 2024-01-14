a78286b2af0843b9180b5cfc0d77709696e897eb
lydiayonas <149570976+lydiayonas@users.noreply.github.com>
2024-01-14 21:49:53 +0300

    Create q4.cpp

---
+++ ETS0848_15_Lydia_Yonas/activity 4.2/string/q4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  string line;
+  cout << "Enter a line of text: ";
+  getline(std::cin, line);
+
+  string token;
+
+  for (size_t i = 0; i < line.length(); i++) {
+    if (line[i] != ' ') {
+      token += line[i];  // Add non-space characters to the current token
+    }
+    else {
+      if (!token.empty()) {
+        cout << token << " ";  // Display the completed token
+        token.clear();  // Clear the current token for the next word
+      }
+    }
+  }
+
+  if (!token.empty()) {
+    cout << token << " ";  // Display the last token if it's not empty
+  }
+
+  cout <<endl;
+
+  return 0;
+}
+
+
+
